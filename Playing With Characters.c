b3feb223011ddd8661f584d77eefa52c1d2e6088
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-08-20 22:06:07 +0530

    Create Playing With Characters.c

---
+++ Playing With Characters.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+ char ch;
+ scanf("%c",&ch);
+ char s[20];
+ scanf("%s",s);
+ char t;
+ scanf("%c",&t);
+ char sen[100];
+ scanf("%[^\n]",sen);
+ printf("%c\n",ch);
+ printf("%s\n",s);
+ printf("%s\n",sen);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
